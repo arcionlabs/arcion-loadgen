d2390a777a1f220f21c03dd934427a980706301d
Robert S Lee <sangkyulee@gmail.com>
2023-02-04 14:27:09 -0500

    fix pg validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -21,11 +21,12 @@ dump_db() {
     local TABLE_NAME=$5
     local REMOVE_COLS=${6:-ts}
 
-    col_name_sql="SELECT column_name FROM information_schema.columns WHERE table_schema='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
-    col_pk_sql="SELECT column_name FROM information_schema.key_column_usage WHERE constraint_name='primary' and table_schema='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
-
 case ${DB_TYPE,,} in
     mysql|mariadb|singlestore)
+        col_name_sql="SELECT column_name FROM information_schema.columns WHERE table_schema='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
+        
+        col_pk_sql="SELECT column_name FROM information_schema.key_column_usage WHERE constraint_name='primary' and table_schema='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
+    
         col_names=$( echo ${col_name_sql} | mysql --skip_column_names -u${DB_ARC_USER} -p${DB_ARC_PW} -h${DB_HOST} -D${DB_ARC_USER} | grep -v ${REMOVE_COLS} | paste -s -d, )
         col_names_pk=$( echo ${col_pk_sql} | mysql --skip_column_names -u${DB_ARC_USER} -p${DB_ARC_PW} -h${DB_HOST} -D${DB_ARC_USER} | grep -v ${REMOVE_COLS} | paste -s -d, )
 
@@ -36,8 +37,14 @@ case ${DB_TYPE,,} in
         echo ${CFG_DIR}/${DB_HOST}.${DB_ARC_USER}.${TABLE_NAME}.tsv
         ;;
     postgresql|cockroach)
-        col_names=$( echo ${col_name_sql} | psql --csv -t postgresql://${DB_ARC_USER}:${DB_ARC_PW}@${DB_HOST}/${DB_ARC_USER} | paste -s -d, )
-        col_names_pk=$( echo ${col_name_sql} | psql --csv -t postgresql://${DB_ARC_USER}:${DB_ARC_PW}@${DB_HOST}/${DB_ARC_USER} | paste -s -d, )
+        col_name_sql="SELECT column_name FROM information_schema.columns WHERE table_catalog='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
+        
+        col_pk_sql="SELECT column_name FROM information_schema.key_column_usage WHERE constraint_name='${TABLE_NAME}_pkey'and table_catalog='${DB_ARC_USER}' and table_name='${TABLE_NAME}' order by ordinal_position;"
+
+        col_names=$( echo ${col_name_sql} | psql --csv -t postgresql://${DB_ARC_USER}:${DB_ARC_PW}@${DB_HOST}/${DB_ARC_USER} | grep -v ${REMOVE_COLS} | paste -s -d, )
+        col_names_pk=$( echo ${col_pk_sql} | psql --csv -t postgresql://${DB_ARC_USER}:${DB_ARC_PW}@${DB_HOST}/${DB_ARC_USER} | grep -v ${REMOVE_COLS} | paste -s -d, )
+
+        echo cols=$col_names pk=$col_names_pk
 
         echo "select ${col_names} from ${TABLE_NAME} order by ${col_names_pk}" | psql --csv -t postgresql://${DB_ARC_USER}:${DB_ARC_PW}@${DB_HOST}/${DB_ARC_USER} > ${CFG_DIR}/${DB_HOST}.${DB_ARC_USER}.${TABLE_NAME}.tsv
 
@@ -54,10 +61,8 @@ dump_db $SRCDB_TYPE $SRCDB_ARC_USER $SRCDB_ARC_PW $SRCDB_HOST usertable
 dump_db $DSTDB_TYPE $DSTDB_ARC_USER $DSTDB_ARC_PW $DSTDB_HOST sbtest1
 dump_db $DSTDB_TYPE $DSTDB_ARC_USER $DSTDB_ARC_PW $DSTDB_HOST usertable
 
+# run the diff
 for TABLE_NAME in sbtest1 usertable; do
-  ls ${CFG_DIR}/*.${TABLE_NAME}.tsv | xargs diff 
+    echo ls ${CFG_DIR}/*.${TABLE_NAME}.tsv \| xargs diff 
+    ls ${CFG_DIR}/*.${TABLE_NAME}.tsv | xargs diff 
 done
-
-# for manual validation, 
-# ls *usertable* | xargs diff
-# ls *sbtest1* | xargs diff
