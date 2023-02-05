299e5314e6c1f03d9e5212974be2a86fc88f63e5
Robert S Lee <sangkyulee@gmail.com>
2023-02-04 23:15:31 -0500

    add port

--- scripts/ini_jdbc.sh
+++ scripts/ini_jdbc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# get the host name and type from the menu
+if [ -f /tmp/ini_menu.sh ]; then . /tmp/ini_menu.sh; fi
+
 # standard source id / password 
 SRCDB_ARC_USER=${SRCDB_ARC_USER:-arcsrc}
 SRCDB_ARC_PW=${SRCDB_ARC_PW:-password}
@@ -9,17 +12,17 @@ DSTDB_ARC_PW=${DSTDB_ARC_PW:-password}
 case "${SRCDB_TYPE,,}" in
   mariadb| mysql|singlestore)
     SRC_JDBC_DRIVER="org.mariadb.jdbc.Driver"
-    SRC_JDBC_URL="jdbc:mysql://${SRCDB_HOST}/${SRCDB_ARC_USER}?permitMysqlScheme"
+    SRC_JDBC_URL="jdbc:mysql://${SRCDB_HOST}:${SRCDB_PORT}/${SRCDB_ARC_USER}?permitMysqlScheme"
     ;;
   postgresql)
     SRC_JDBC_DRIVER="org.postgresql.Driver"
-    SRC_JDBC_URL="jdbc:postgresql://${SRCDB_HOST}/${SRCDB_ARC_USER}?"   
-    SRC_PG_URL="postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}/${SRCDB_ARC_USER}"
+    SRC_JDBC_URL="jdbc:postgresql://${SRCDB_HOST}:${SRCDB_PORT}/${SRCDB_ARC_USER}?"   
+    SRC_PG_URL="postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}:${SRCDB_PORT}/${SRCDB_ARC_USER}"
     ;;
   cockroach)
     SRC_JDBC_DRIVER="org.postgresql.Driver"
     SRC_JDBC_URL="jdbc:postgresql://${SRCDB_HOST}:26257/${SRCDB_ARC_USER}?autoReconnect=true&sslmode=disable&ssl=false"   
-    SRC_PG_URL="postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}/${SRCDB_ARC_USER}"
+    SRC_PG_URL="postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}:${SRCDB_PORT}/${SRCDB_ARC_USER}"
     ;; 
   *)
     echo "$0: SRCDB_TYPE: ${SRCDB_TYPE} need to code support"
@@ -29,17 +32,17 @@ esac
 case "${DSTDB_TYPE,,}" in
   mariadb| mysql|singlestore)
     DST_JDBC_DRIVER="org.mariadb.jdbc.Driver"
-    DST_JDBC_URL="jdbc:mysql://${DSTDB_HOST}/${DSTDB_ARC_USER}?permitMysqlScheme"
+    DST_JDBC_URL="jdbc:mysql://${DSTDB_HOST}:${DSTDB_PORT}/${DSTDB_ARC_USER}?permitMysqlScheme"
     ;;
   postgresql)
     DST_JDBC_DRIVER="org.postgresql.Driver"
-    DST_JDBC_URL="jdbc:postgresql://${DSTDB_HOST}/${ARCDST_USER}?"   
-    DST_PG_URL="postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}/${DSTDB_ARC_USER}"
+    DST_JDBC_URL="jdbc:postgresql://${DSTDB_HOST}:${DSTDB_PORT}/${ARCDST_USER}?"   
+    DST_PG_URL="postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}:${DSTDB_PORT}/${DSTDB_ARC_USER}"
     ;;
   cockroach)
     DST_JDBC_DRIVER="org.postgresql.Driver"
-    DST_JDBC_URL="jdbc:postgresql://${DSTDB_HOST}:26257/${DSTDB_ARC_USER}?autoReconnect=true&sslmode=disable&ssl=false"   
-    DST_PG_URL="postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}/${DSTDB_ARC_USER}"
+    DST_JDBC_URL="jdbc:postgresql://${DSTDB_HOST}::${DSTDB_PORT}/${DSTDB_ARC_USER}?autoReconnect=true&sslmode=disable&ssl=false"   
+    DST_PG_URL="postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}:${DSTDB_PORT}/${DSTDB_ARC_USER}"
     ;; 
   *)
     echo "$0: DSTDB_TYPE: ${DSTDB_TYPE} need to code support"
--- scripts/verify.sysbench.sh
+++ scripts/verify.sysbench.sh
@@ -23,12 +23,12 @@ if [ "$?" != "0" ]; then echo "pane .0 does not exist"; exit 1; fi
 case ${SRCDB_TYPE,,} in
     mysql|mariadb|singlestore)
         echo ${SRCDB_HOST} ${SRCDB_TYPE}    
-        tmux send-keys -t :3.0 "watch -n 1 \"mysql -t -u${SRCDB_ARC_USER} -h${SRCDB_HOST} -p${SRCDB_ARC_PW} -D${SRCDB_ARC_USER} -e 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter
+        tmux send-keys -t :3.0 "watch -n 1 \"mysql -t -u${SRCDB_ARC_USER} -h${SRCDB_HOST} -p${SRCDB_ARC_PW} -D${SRCDB_ARC_USER} -P${SRCDB_PORT} -e 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter
     ;;
     postgresql|cockroach)
         echo ${SRCDB_HOST} ${SRCDB_TYPE}    
         echo ${SRCDB_HOST} ${SRCDB_TYPE}   
-        tmux send-keys -t :3.0 "watch -n 1 \"psql postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}/${SRCDB_ARC_USER} -c 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter        
+        tmux send-keys -t :3.0 "watch -n 1 \"psql postgresql://${SRCDB_ARC_USER}:${SRCDB_ARC_PW}@${SRCDB_HOST}:${SRCDB_PORT}/${SRCDB_ARC_USER} -c 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter        
         ;; 
     *)
         echo "Error: ${DSTDB_TYPE} needs to be supproted"
@@ -42,14 +42,14 @@ case ${DSTDB_TYPE,,} in
     mysql|mariadb|singlestore)
         echo ${DSTDB_HOST} ${DSTDB_TYPE}   
         # ts2 exists? 
-        [ ! -z "$( mysql -t -u${DSTDB_ARC_USER} -h${DSTDB_HOST} -p${DSTDB_ARC_PW} -D${DSTDB_ARC_USER} -e 'desc sbtest1' | grep ts2 )" ] && TS2_ORD=',ts2 desc' && TS2_SEL=',ts2-ts'
+        [ ! -z "$( mysql -t -u${DSTDB_ARC_USER} -h${DSTDB_HOST} -p${DSTDB_ARC_PW} -D${DSTDB_ARC_USER} -P${DSTDB_PORT} -e 'desc sbtest1' | grep ts2 )" ] && TS2_ORD=',ts2 desc' && TS2_SEL=',ts2-ts'
         # show 
-        tmux send-keys -t :3.1 "watch -n 1 \"mysql -t -u${DSTDB_ARC_USER} -h${DSTDB_HOST} -p${DSTDB_ARC_PW} -D${DSTDB_ARC_USER} -e 'select id,ts ${TS2_SEL} from sbtest1 order by ts desc ${TS2_ORD},id asc limit 20;'\"" enter
+        tmux send-keys -t :3.1 "watch -n 1 \"mysql -t -u${DSTDB_ARC_USER} -h${DSTDB_HOST} -p${DSTDB_ARC_PW} -D${DSTDB_ARC_USER} -P${DSTDB_PORT} -e 'select id,ts ${TS2_SEL} from sbtest1 order by ts desc ${TS2_ORD},id asc limit 20;'\"" enter
     ;;
     postgresql|cockroach)
         echo ${DSTDB_HOST} ${DSTDB_TYPE}  
         echo ${DSTDB_HOST} ${DSTDB_TYPE}    
-        tmux send-keys -t :3.1 "watch -n 1 \"psql postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}/${DSTDB_ARC_USER} -c 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter            
+        tmux send-keys -t :3.1 "watch -n 1 \"psql postgresql://${DSTDB_ARC_USER}:${DSTDB_ARC_PW}@${DSTDB_HOST}:${DSTDB_PORT}/${DSTDB_ARC_USER} -c 'select id,ts from sbtest1 order by ts desc,id asc limit 20;'\"" enter            
         ;; 
     *)
         echo "Error: ${DSTDB_TYPE} needs to be supproted"
