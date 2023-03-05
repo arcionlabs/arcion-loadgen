64a93d546a7c1df47e49c73a5a11002189c3ff7a
robert s lee <sangkyulee@gmail.com>
2023-03-04 20:45:11 -0500

    modified:   load-generator/Dockerfile.arcdemo 	modified:   scripts/mysql/src.init.user.sql

--- scripts/mysql/src.init.user.sql
+++ scripts/mysql/src.init.user.sql
@@ -1,11 +1,11 @@
 -- create arcsrc for retrivial
 drop table replicate_io_cdc_heartbeat;
-CREATE TABLE if replicate_io_cdc_heartbeat(
+CREATE TABLE replicate_io_cdc_heartbeat(
   timestamp BIGINT NOT NULL,
   PRIMARY KEY(timestamp)
 );
 
-drop table sbtest1;
+-- drop table sbtest1;
 CREATE TABLE sbtest1(
 	id INTEGER,
 	k INTEGER DEFAULT '0' NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE sbtest1(
 );
 
 -- ts is used for snapshot delta. 
-drop table usertable;
+-- drop table usertable;
 CREATE TABLE usertable (
 	ycsb_key VARCHAR(255) PRIMARY KEY,
 	field0 TEXT, field1 TEXT,
