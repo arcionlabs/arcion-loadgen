2d65576776bfdf5d6c60ad99b94495e9a9be0d9e
robert s lee <sangkyulee@gmail.com>
2023-03-28 21:01:29 -0400

    archive sysbench 	new file:   scripts/bin/sysbench.sh 	new file:   scripts/cockroach/dst_applier.yaml 	new file:   scripts/utils/benchbase/bb_no_batchrewrite.csv 	new file:   scripts/utils/benchbase/bbtables.csv

---
+++ scripts/bin/sysbench.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# get the setting from the menu
+if [ -f /tmp/ini_menu.sh ]; then . /tmp/ini_menu.sh; fi
+
+# env
+SCRIPTS_DIR=${SCRIPTS_DIR:-/scripts}
+ARCION_HOME=${ARCION_HOME:-/arcion}
+if [ -d ${ARCION_HOME}/replicant-cli ]; then ARCION_HOME=${ARCION_HOME}/replicant-cli; fi
+
+# bail if sysbench is not installed
+which sysbench > /dev/null 2>&1
+if [ $? != "0" ]; then
+    echo "Warning: sysbench not found.  Try 'sudo apt-get install sysbench' or 'brew install sysbench'"
+    exit 1
+fi
+
+# finally, run with lowercase SRCDB_TYPE
+case ${SRCDB_GRP,,} in
+    mysql)
+        echo ${SRCDB_HOST} ${SRCDB_GRP}
+        sysbench oltp_read_write \
+        --rand-type=uniform \
+        --rate=${workload_rate} \
+        --report-interval=10 \
+        --mysql-host=${SRCDB_HOST} \
+        --auto_inc=off \
+        --db-driver=mysql \
+        --mysql-user=${SRCDB_ARC_USER} \
+        --mysql-password=${SRCDB_ARC_PW} \
+        --mysql-db=${SRCDB_ARC_USER} \
+        --mysql-port=${SRCDB_PORT} \
+        --time=${workload_timer} \
+        --threads=${workload_threads} \
+        --tables=${workload_size_factor} \
+        run
+    ;;
+    postgresql)
+        echo ${SRCDB_HOST} ${SRCDB_GRP}
+        sysbench oltp_read_write \
+        --rand-type=uniform \
+        --rate=${workload_rate} \
+        --report-interval=10 \
+        --pgsql-host=${SRCDB_HOST} \
+        --auto_inc=off \
+        --db-driver=pgsql \
+        --pgsql-user=${SRCDB_ARC_USER} \
+        --pgsql-password=${SRCDB_ARC_PW} \
+        --pgsql-db=${SRCDB_ARC_USER} \
+        --pgsql-port=${SRCDB_PORT} \
+        --time=${workload_timer} \
+        --threads=${workload_threads} \
+        --tables=${workload_size_factor} \
+    run
+        ;;
+    *)
+        echo "sysbench.sh: ${SRCDB_GRP} needs to be supported"
+        ;;
+esac
