30138dac594c7351112cfc9a43db641909768833
robert s lee <sangkyulee@gmail.com>
2023-04-04 17:21:17 -0400

    support oracle 	new file:   scripts/oracle/dst.init.user.sql 	new file:   scripts/oracle/src.init.user.sql

---
+++ scripts/oracle/dst.init.user.sql
@@ -0,0 +1,9 @@
+-- ts is used for snapshot delta. 
+CREATE TABLE if not exists theusertable (
+	ycsb_key VARCHAR(255) PRIMARY KEY,
+	field0 TEXT, field1 TEXT,
+	field2 TEXT, field3 TEXT,
+	field4 TEXT, field5 TEXT,
+	field6 TEXT, field7 TEXT,
+	field8 TEXT, field9 TEXT
+);
