3ab6123c3a170e14fe09c51da9fe3bb47a964707
robert s lee <sangkyulee@gmail.com>
2023-04-22 21:47:15 -0400

    classpath for oracle jdbc when running oracle ycsb 	modified:   scripts/utils/ycsb/setenv.sh

--- scripts/utils/ycsb/setenv.sh
+++ scripts/utils/ycsb/setenv.sh
@@ -15,4 +15,9 @@ if [ -d ${ARCION_HOME}/replicant-cli ]; then ARCION_HOME=${ARCION_HOME}/replican
 # informix jdbc-*jar
 # informix bson-*jar
 # oracle ojdbc-*.jar
-CLASSPATH=$(ls ${ARCION_HOME}/lib/maria* ${ARCION_HOME}/lib/post* ${ARCION_HOME}/lib/mongodb* ${ARCION_HOME}/lib/mssql* ${ARCION_HOME}/lib/db2* ${ARCION_HOME}/lib/jconn4*jar ${ARCION_HOME}/lib/jdbc-*jar ${ARCION_HOME}/lib/bson-*jar ${ARCION_HOME}/lib/ojdbc8*jar | paste -sd:)
+# oracle jar does does not play nice with mysql
+if [ "${SRCDB_GRP}" = "oracle" ]; then
+  CLASSPATH=$(ls ${ARCION_HOME}/lib/ojdbc8*jar | paste -sd:)
+else
+  CLASSPATH=$(ls ${ARCION_HOME}/lib/maria* ${ARCION_HOME}/lib/post* ${ARCION_HOME}/lib/mongodb* ${ARCION_HOME}/lib/mssql* ${ARCION_HOME}/lib/db2* ${ARCION_HOME}/lib/jconn4*jar ${ARCION_HOME}/lib/jdbc-*jar ${ARCION_HOME}/lib/bson-*jar | paste -sd:)
+fi
