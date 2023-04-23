fd95e933d067f4005510ce8a6345d62e393f0b2b
robert s lee <sangkyulee@gmail.com>
2023-04-23 09:53:06 -0400

    reorg docs 	deleted:    docs/README.mysql.md 	modified:   scripts/singlestore/dst_applier.yaml 	modified:   scripts/singlestore/src.init.user.sql 	modified:   scripts/singlestore/src_extractor.yaml 	modified:   scripts/singlestore/src_filter.yaml 	modified:   scripts/utils/mysql/src_extractor.yaml 	docs/arcion/db-name-convention.md 	docs/mysql/README.mysql.md

--- scripts/singlestore/src.init.user.sql
+++ scripts/singlestore/src.init.user.sql
@@ -1,13 +1,11 @@
 -- ts is used for snapshot delta/
 CREATE TABLE if not exists theusertable (
-    ycsb_key int,
+    ycsb_key int primary key,
     field0 TEXT, field1 TEXT,
     field2 TEXT, field3 TEXT,
     field4 TEXT, field5 TEXT,
     field6 TEXT, field7 TEXT,
     field8 TEXT, field9 TEXT,
 	ts TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-	key (ts) using hash,
-    sort key (ycsb_key),
-    shard key (ycsb_key)
+	index (ts)
 );
