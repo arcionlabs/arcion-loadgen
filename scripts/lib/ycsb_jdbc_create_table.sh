9d9ae62deaf55cb164daf20e6625715453702bbe
robert s lee <sangkyulee@gmail.com>
2023-04-24 15:10:51 -0400

    move ycsb table def to ycsb 	new file:   scripts/lib/splitkey_utils.sh 	new file:   scripts/lib/ycsb_jdbc_create_table.sh 	new file:   scripts/postgresql/dst.init.user.sql 	new file:   scripts/postgresql/src.init.user.sql

---
+++ scripts/lib/ycsb_jdbc_create_table.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+ycsb_create_table() {
+    if [ -z "${db_type,,}" ]; then
+        echo "Error: db_type is not set." >&2
+        return 1
+    fi
+
+    case "${db_type,,}" in
+        mysql,mariadb,singlestore,cockroach)
+            cat <<EOF
+CREATE TABLE if not exists theusertable (
+    ycsb_key int primary key,
+    field0 TEXT, field1 TEXT,
+    field2 TEXT, field3 TEXT,
+    field4 TEXT, field5 TEXT,
+    field6 TEXT, field7 TEXT,
+    field8 TEXT, field9 TEXT,
+    ts TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    index (ts)
+);
+EOF
+        ;;    
+        yugabytesql|postgresql)
+            cat <<'EOF'
+CREATE TABLE if not exists theusertable (
+    ycsb_key int primary key,
+    field0 TEXT, field1 TEXT,
+    field2 TEXT, field3 TEXT,
+    field4 TEXT, field5 TEXT,
+    field6 TEXT, field7 TEXT,
+    field8 TEXT, field9 TEXT,
+    ts TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6)
+); 
+CREATE INDEX IF NOT EXISTS theusertable_ts ON theusertable(ts);
+
+-- source trigger
+
+CREATE OR REPLACE FUNCTION update_ts()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.ts = CURRENT_TIMESTAMP(6);
+    RETURN NEW;
+END; $$ language 'plpgsql';
+
+CREATE TRIGGER update_ts_on_theusertable BEFORE UPDATE ON theusertable FOR EACH ROW EXECUTE PROCEDURE update_ts();
+go
+EOF
+        ;;
+        *)
+            cat <<EOF
+db_sql="CREATE TABLE if not exists theusertable (
+    ycsb_key int primary key,
+    field0 TEXT, field1 TEXT,
+    field2 TEXT, field3 TEXT,
+    field4 TEXT, field5 TEXT,
+    field6 TEXT, field7 TEXT,
+    field8 TEXT, field9 TEXT,
+    ts TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6),
+); 
+create index if not exists on theusertable (ts);"
+EOF
+    ;;   
+    esac
+}
