3c634660e75166febbb1f8e2f43361c122decbcb
robert-s-lee <sangkyulee@gmail.com>
2023-06-02 15:29:42 -0400

    increase chunk size on pg

--- scripts/cockroach/dst.init.root.sql
+++ scripts/cockroach/dst.init.root.sql
@@ -1,3 +1,8 @@
+-- create user and db
 CREATE USER ${DSTDB_ARC_USER}; -- PASSWORD 'password';
 ALTER USER ${DSTDB_ARC_USER} CREATEDB;
 CREATE DATABASE ${DSTDB_DB} WITH OWNER ${DSTDB_ARC_USER}; -- ENCODING 'UTF8';
+
+-- not required but recommneded by cockroachdb
+ALTER ROLE ${DSTDB_ARC_USER} SET copy_from_retries_enabled = true;
+ALTER ROLE ${DSTDB_ARC_USER} SET copy_from_atomic_enabled = false;
