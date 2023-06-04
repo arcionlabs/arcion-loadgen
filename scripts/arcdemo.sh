a792e0661f76f65c5c8f755929d6d2bdb2a21748
robert-s-lee <sangkyulee@gmail.com>
2023-06-04 16:09:04 -0400

    check for license in general.yaml

--- scripts/arcdemo.sh
+++ scripts/arcdemo.sh
@@ -45,9 +45,22 @@ if [ ! -f "$ARCION_HOME/bin/replicant" ]; then
   exit 1
 fi
 
-if [ ! -f "$ARCION_HOME/replicant.lic" ]; then
-  echo "$ARCION_HOME/replicantlic not found." >&2
+if [ -f "$ARCION_HOME/replicant.lic" ]; then
+  echo "$ARCION_HOME/replicant.lic not found." >&2
   exit 1
+elif [ -f "$SCRIPTS_DIR/utils/arcion/general.yaml" ]; then
+  echo "checking $SCRIPTS_DIR/utils/arcion/general.yaml"
+  license_path=$( cat $SCRIPTS_DIR/utils/arcion/general.yaml | \
+    yq -r '."license-path"')
+  echo "license_path=${license_path}"
+  if [ -n "${license_path}" ] && [ -f "${license_path}" ]; then 
+    echo "license ${license_path} exists"
+  else
+    echo "Error: license ${license_path} does not exists"
+    exit 1
+  fi
+else
+  echo "Error: $ARCION_HOME/replicant.lic nor $SCRIPTS_DIR/utils/arcion/general.yaml exists"
 fi
 
 export ARCION_VER=$($ARCION_HOME/bin/replicant version 2>/dev/null | grep "^Version" | awk '{print $2}')
