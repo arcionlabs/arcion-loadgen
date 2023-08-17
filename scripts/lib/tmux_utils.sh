5febec14fcbee33d9e90ebde1b1c4d40df829d08
Robert S Lee <sangkyulee@gmail.com>
2023-08-17 16:10:19 -0400

    move into log dir

--- scripts/lib/tmux_utils.sh
+++ scripts/lib/tmux_utils.sh
@@ -49,7 +49,8 @@ tmux_show_ycsb() {
 }
 
 tmux_show_errorlog() {
-    tmux send-keys -t ${TMUX_SESSION}:0.3 "clear; figlet -t errlog; while [ ! -f ${ARCION_LOG}/${LOG_ID}/error_trace.log ]; do sleep 5; done; tail -f ${ARCION_LOG}/${LOG_ID}/error_trace.log" Enter
+    tmux send-keys -t ${TMUX_SESSION}:0.3 "figlet -t errlog; while [ ! -f ${ARCION_LOG}/${LOG_ID}/error_trace.log ]; do sleep 1; done; cd ${ARCION_LOG}/${LOG_ID}; tail -f error_trace.log" Enter
+
 }
 
 tmux_show_verification() {
@@ -74,8 +75,7 @@ tmux_show_yaml()  {
 tmux_show_trace()  {
     local TMUX_SESSION=${1}
 
-    tmux send-keys -t ${TMUX_SESSION}:2.0 "while [ ! -f ${ARCION_LOG}/${LOG_ID}/trace.log ]; do sleep 5; done; tail -f ${ARCION_LOG}/${LOG_ID}/trace.log" Enter
-    tmux send-keys -t ${TMUX_SESSION}:2.0 ":E" Enter 
+    tmux send-keys -t ${TMUX_SESSION}:2.0 "while [ ! -f ${ARCION_LOG}/${LOG_ID}/trace.log ]; do sleep 1; done; cd ${ARCION_LOG}/${LOG_ID}; tail -f trace.log" Enter
 }
 
 tmux_show_console()  {
