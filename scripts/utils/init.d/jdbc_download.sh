3a55c50d264491d67df3bd52fa49eef2dc901339
Robert Lee <sangkyulee@gmail.com>
2023-12-02 10:06:58 -0500

    download deltalake and lakehouse jdbc jars

--- scripts/utils/init.d/jdbc_download.sh
+++ scripts/utils/init.d/jdbc_download.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+# vertica
+if [ ! -f /opt/stage/libs/vertica-jdbc-23.4.0-0.jar ]; then
+    pushd /opt/stage/libs >/dev/null
+    wget https://repo1.maven.org/maven2/com/vertica/jdbc/vertica-jdbc/23.4.0-0/vertica-jdbc-23.4.0-0.jar
+    popd >/dev/null
+else
+    echo "vertica /opt/stage/libs/vertica-jdbc-23.4.0-0.jar found"
+fi
+
+# db2
+if [ ! -f /opt/stage/libs/jcc-11.5.9.0.jar ]; then
+    pushd /opt/stage/libs >/dev/null
+    wget https://repo1.maven.org/maven2/com/ibm/db2/jcc/11.5.9.0/jcc-11.5.9.0.jar
+    popd >/dev/null
+else
+    echo "db2 /opt/stage/libs/jcc-11.5.9.0.jar found"
+fi
+
+#informix
+if [ ! -f /opt/stage/libs/jdbc-4.50.10.jar ]; then
+    pushd /opt/stage/libs >/dev/null
+    wget https://repo1.maven.org/maven2/com/ibm/informix/jdbc/4.50.10/jdbc-4.50.10.jar
+    popd >/dev/null
+else
+    echo "informix /opt/stage/libs/jdbc-4.50.10.jar found"
+fi
+
 # download oracle jdbc if not there
 if [ ! -f /opt/stage/libs/ojdbc8.jar ]; then
     pushd /opt/stage/libs >/dev/null
     curl -O --location https://download.oracle.com/otn-pub/otn_software/jdbc/1815/ojdbc8.jar
     popd >/dev/null
 else
-    echo "/opt/stage/libs/ojdbc8.jar found"
+    echo "oracle /opt/stage/libs/ojdbc8.jar found"
 fi
 
 # download GoogleBigQuery jdbc if not there
@@ -17,7 +44,28 @@ if [ ! -f /opt/stage/libs/GoogleBigQueryJDBC42.jar ]; then
     rm SimbaJDBCDriverforGoogleBigQuery42_1.2.25.1029.zip
     popd >/dev/null
 else
-    echo "/opt/stage/libs/GoogleBigQueryJDBC42.jar found"
+    echo "google big query /opt/stage/libs/GoogleBigQueryJDBC42.jar found"
+fi
+
+# deltalake
+if [ ! -f /opt/stage/libs/SparkJDBC42.jar ]; then
+    pushd /opt/stage/libs >/dev/null
+    wget https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/jdbc/2.6.22/SimbaSparkJDBC42-2.6.22.1040.zip
+    unzip -q SimbaSparkJDBC42-2.6.22.1040.zip
+    rm SimbaSparkJDBC42-2.6.22.1040.zip
+    popd >/dev/null
+else
+    echo "deltalake /opt/stage/libs/SparkJDBC42.jar found"
+fi
+
+# lakehouse (unity catalog)
+if [ ! -f /opt/stage/libs/DatabricksJDBC42.jar ]; then
+    pushd /opt/stage/libs >/dev/null
+    wget https://repo1.maven.org/maven2/com/databricks/databricks-jdbc/2.6.34/databricks-jdbc-2.6.34.jar
+    mv databricks-jdbc-2.6.34.jar DatabricksJDBC42.jar
+    popd >/dev/null
+else
+    echo "lakehouse  /opt/stage/libs/DatabricksJDBC42.jar found"
 fi
 
 # download log4j
@@ -26,7 +74,7 @@ if [ ! -f /opt/stage/libs/log4j-1.2.17.jar ]; then
     curl -O --location https://repo1.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar
     popd >/dev/null
 else
-    echo "/opt/stage/libs/log4j-1.2.17.jar found"
+    echo "log4j /opt/stage/libs/log4j-1.2.17.jar found"
 fi
 
 # copy 
